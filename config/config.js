be6e830fbf5406f21e5b9851d687813993fc2aec
Aqil Rahmatullah <aqilr54@gmail.com>
2024-06-11 23:41:42 +0800

    fix db

--- config/config.js
+++ config/config.js
@@ -38,7 +38,7 @@ export default {
   test: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || null,
-    database: process.env.TEST_DB_NAME || 'database_test',
+    database: process.env.DB_NAME || 'database_test',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306,
@@ -46,7 +46,7 @@ export default {
   production: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || null,
-    database: process.env.PROD_DB_NAME || 'database_production',
+    database: process.env.DB_NAME || 'database_production',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306,
