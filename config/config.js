8afaf6149cf57f1b552815aefd67c5ced69bf13d
Aqil Rahmatullah <aqilr54@gmail.com>
2024-06-11 23:38:28 +0800

    fix db

---
+++ config/config.js
@@ -0,0 +1,54 @@
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "new2_db",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'new2_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+  },
+  test: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.TEST_DB_NAME || 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+  },
+  production: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.PROD_DB_NAME || 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+  },
+};
