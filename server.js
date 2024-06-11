bfc81efe1a77b55e303b5b57de4dd19bdab954ae
Aqil Rahmatullah <aqilr54@gmail.com>
2024-06-11 21:27:06 +0800

    add: fix server

--- server.js
+++ server.js
@@ -11,19 +11,41 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-try {
-  await db.authenticate();
-  await initDatabase();
-  console.log('Database Connected...');
-} catch (error) {
-  console.error(error);
-}
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    await initDatabase();
+    console.log('Database Connected...');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1); // Exit process with failure
+  }
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(cookieParser());
-app.use(express.json());
-app.use(fileUpload());
-app.use(express.static('public'));
-app.use(router);
+  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(fileUpload());
+  app.use(express.static('public'));
+  app.use(router);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
+
+startServer();
+
+// try {
+//   await db.authenticate();
+//   await initDatabase();
+//   console.log('Database Connected...');
+// } catch (error) {
+//   console.error(error);
+// }
+
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(fileUpload());
+// app.use(express.static('public'));
+// app.use(router);
+
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
