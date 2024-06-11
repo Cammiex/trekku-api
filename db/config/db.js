f6ef2b11ddfb38ff20f6710fe2f61b7ef59dc640
Aqil Rahmatullah <aqilr54@gmail.com>
2024-06-11 21:32:39 +0800

    add: fix server v2

--- db/config/db.js
+++ db/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.PASSWORD,
+  process.env.DB_PASSWORD,
   {
     host: `${process.env.DB_HOST}`,
     dialect: 'mysql',
